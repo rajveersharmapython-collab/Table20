346be8e366617ea517b4e09c79b18781a65ccd3f
Rajveer Sharma Python <rajveersharma.python@gmail.com>
2026-01-31 19:30:27 +0530

    2nd commit

---
+++ 01_Table_of_20.py
@@ -0,0 +1,5 @@
+
+Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] #List
+for multiply in Numbers: #For Loop
+    txt = "20 *"    #String
+    print( format(txt), (multiply), "=", (multiply) * 20) #print function
